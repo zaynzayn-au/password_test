99cb4c20a45893227bbaefd2a721a896674c70a4
Zayn Hwang <yz.huang1218@gmail.com>
2020-04-02 23:49:30 +1100

    while循环 密码尝试三次版本

---
+++ password.py
@@ -0,0 +1,14 @@
+#密码重试练习
+user_password = 'a12345'
+x = 4
+
+while True:
+	password = input('请输入您的密码：')
+	if password == user_password:
+		print('登录成功！')
+		break
+	else:
+		x = x - 1
+		print('登录失败，你还有',x,'次机会')
+		if x == 1:
+			break
