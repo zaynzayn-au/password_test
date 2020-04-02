26a48e6971ec4b8ac2918e8395ac8a2e544cdded
Zayn Hwang <yz.huang1218@gmail.com>
2020-04-02 23:53:08 +1100

    while的条件版本

--- password.py
+++ password.py
@@ -1,8 +1,8 @@
 #密码重试练习
 user_password = 'a12345'
-x = 4
+x = 3
 
-while True:
+while x > 0:
 	password = input('请输入您的密码：')
 	if password == user_password:
 		print('登录成功！')
@@ -10,5 +10,3 @@ while True:
 	else:
 		x = x - 1
 		print('登录失败，你还有',x,'次机会')
-		if x == 1:
-			break
